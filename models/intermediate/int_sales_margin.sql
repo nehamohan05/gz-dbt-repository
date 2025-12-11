2aa1db3d689d36142d68c614170486b377f69e89
Neha Mohan <neha5apr84@gmail.com>
2025-12-11 10:41:55 +0000

    Commit int_orders_marginn

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,12 @@
   SELECT
-      products_id,
-      date_date,
-      orders_id,
-      revenue,
-      quantity,
-      purchase_price,
+      p.products_id,
+      s.date_date,
+      s.orders_id,
+      s.revenue,
+      s.quantity,
+      p.purchase_price,
       ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
       ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
   FROM {{ref("stg_raw__sales")}} s
   LEFT JOIN {{ref("stg_raw__product")}} p
-      USING (products_id)
+      ON (s.pdt_id=p.products_id)
